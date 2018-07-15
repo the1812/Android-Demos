9ccf9214f233b8a34b09a7a2f81a9929a0dadf85
the1812 <the1812@outlook.com>
2018-07-15 12:19:01 +0800

    Add files and shared preferences demo.

--- app/src/main/java/com/helloworld/androiddemo/MainActivity.kt
+++ app/src/main/java/com/helloworld/androiddemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.helloworld.androiddemo.activities.ActivitiesActivity
 import com.helloworld.androiddemo.broadcasts.BroadcastsActivity
 import com.helloworld.androiddemo.components.ComponentsActivity
+import com.helloworld.androiddemo.files.FilesActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
@@ -22,7 +23,8 @@ class MainActivity : AppCompatActivity()
         navigation.items = mapOf(
             getString(R.string.activities) to ActivitiesActivity::class.java,
             getString(R.string.components) to ComponentsActivity::class.java,
-            getString(R.string.broadcasts) to BroadcastsActivity::class.java
+            getString(R.string.broadcasts) to BroadcastsActivity::class.java,
+            getString(R.string.files) to FilesActivity::class.java
         )
         navigation.createNavigation()
     }
---
+++ app/src/main/java/com/helloworld/androiddemo/files/FilesActivity.kt
@@ -0,0 +1,71 @@
+package com.helloworld.androiddemo.files
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.helloworld.androiddemo.R
+import kotlinx.android.synthetic.main.activity_files.*
+import java.io.File
+
+class FilesActivity : AppCompatActivity()
+{
+    private fun toast(text: String) = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_files)
+        createFilesHandler()
+        createPreferencesHandler()
+    }
+
+    private val preferenceKey = "preference_demo"
+    private fun createPreferencesHandler()
+    {
+        buttonSaveToPreferences.setOnClickListener {
+            val editor = getPreferences(Context.MODE_PRIVATE).edit()
+            editor.putString(preferenceKey, textPreferences.text.toString())
+            editor.apply()
+            textPreferences.text.clear()
+            toast("Saved preference \"$preferenceKey\"")
+        }
+        buttonReadPreferences.setOnClickListener {
+            val preferences = getPreferences(Context.MODE_PRIVATE)
+            val data = preferences.getString(preferenceKey, "")
+            textPreferences.setText(data)
+            if (data.isEmpty())
+            {
+                toast("No text found in preferences.")
+            }
+            else
+            {
+                toast("Read preference from \"$preferenceKey\"")
+            }
+        }
+    }
+
+    private val fileName = "file_demo.txt"
+    private fun createFilesHandler()
+    {
+        buttonSaveToFile.setOnClickListener {
+            val file = File(filesDir, fileName)
+            file.writeText(textFile.text.toString())
+            textFile.text.clear()
+            toast("Saved to $fileName.")
+        }
+        buttonReadFile.setOnClickListener {
+            val file = File(filesDir, fileName)
+            val text = file.readText()
+            textFile.setText(text)
+            if (text.isEmpty())
+            {
+                toast("No text found in file.")
+            }
+            else
+            {
+                toast("Read file from $fileName.")
+            }
+        }
+    }
+}
